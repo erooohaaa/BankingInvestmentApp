6605b0f15729883e75112fbf2070aebe1e2e707c
Yerkanat <erkanatm249@gmail.com>
2025-10-12 16:16:02 +0500

    2nd commit for the account methods and create an empty class of facade

--- src/accounts/Account.java
+++ src/accounts/Account.java
@@ -1,4 +1,10 @@
 package accounts;
 
 public interface Account {
+    void deposit(double amount);
+    void withdraw(double amount);
+    double getBalance();
+    String getDescription();
+    void close();
+
 }
